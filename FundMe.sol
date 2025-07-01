6e3ff1bae221a456d78d633b6cba8507631693dd
ifqygazhar <ifqygazhar@gmail.com>
2025-07-01 20:36:11 +0700

    feat:implement gass efision

--- FundMe.sol
+++ FundMe.sol
@@ -3,22 +3,27 @@ pragma solidity ^0.8.18;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
-    address public owner;
+    //using immutable to reduce the gass
+    address public immutable i_owner;
 
     constructor () {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     using PriceConverter for uint256;
 
-    uint256 public minimalAmmoundUsd = 5e18;
+    //using const to reduce the gass
+    uint256 public constant MINIMUM_USD = 5e18;
+
     address[] public listOfFunds;
     mapping(address funder => uint256 amountFunded) public addressToAmount;
 
     function fund() public payable {
         // uint256 currentConversion = PriceConverter.getConversionRate(msg.value);
-        require(msg.value.getConversionRate() >= minimalAmmoundUsd,"didn't send enough of funds"); // 1 ETH 
+        require(msg.value.getConversionRate() >= MINIMUM_USD,"didn't send enough of funds"); // 1 ETH 
         listOfFunds.push(msg.sender);
         addressToAmount[msg.sender] += msg.value;
     }
@@ -44,7 +49,8 @@ contract FundMe {
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Not the owner of this contract");
+        // require(msg.sender == i_owner, "Not the owner of this contract");
+        if(msg.sender != i_owner) revert NotOwner();
         _;
     }   
 }
