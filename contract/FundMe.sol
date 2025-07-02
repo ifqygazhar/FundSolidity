81f203accb84ec580b4f6c18fab3f02a8112927a
ifqygazhar <ifqygazhar@gmail.com>
2025-07-02 08:06:37 +0700

    feat:trying deploy into zksync testnet network

--- contract/FundMe.sol
+++ contract/FundMe.sol
@@ -59,4 +59,12 @@ contract FundMe {
         if(msg.sender != i_owner) revert NotOwner();
         _;
     }   
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
 }
